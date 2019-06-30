6ab58c6ffccd6747faae42ddfdcb8477fbd25c0f
Rob Thuleen <rob.thuleen@gmail.com>
2019-06-30 08:57:46 -0700

    Move turnValues and compass to module constants

--- Rover.js
+++ Rover.js
@@ -1,8 +1,13 @@
+const COMPASS = 'NESW'; // Compass headings in clockwise order from North
+const TURN_VALUES = {
+    'R': 1,  // A right turn is one position
+    'L': 3   // A left turn is the same as 3 right turns.  This allows modulus math for turn index
+
+}
 class Rover {
     constructor(position, grid) {
         [this.xMax, this.yMax] = grid.split(' ');
         [this.x, this.y, this.heading] = position.split(' ');
-        this.compass = 'NESW'; // Compass headings in clockwise order from North
     }
 
     execute(commands) {
@@ -13,13 +18,9 @@ class Rover {
     }
 
     turn(turn) {
-        let turnValues = {
-            'L': 3,
-            'R': 1
-        }
-        let turnValue =  turnValues[turn] || 0; // Get the index of the current heading from the compass
-        let headingIndex = (this.compass.indexOf(this.heading) + turnValue) % 4; // Compute the lookup value for the new heading
-        this.heading = this.compass[headingIndex];
+        let turnValue =  TURN_VALUES[turn] || 0; // Get the index of the current heading from the compass
+        let headingIndex = (COMPASS.indexOf(this.heading) + turnValue) % 4; // Compute the lookup value for the new heading
+        this.heading = COMPASS[headingIndex];
     }
 
     move() {
